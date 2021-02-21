16944be1ce70f2bd23a9534af33c650dd7f4f9cf
MJHC <magnus@bonusfamilien.dk>
2021-02-21 16:45:15 +0100

    Changed the jquery to js and fixed forms

--- js/loginForm.js
+++ js/loginForm.js
@@ -1,47 +1,9 @@
-console.log("loginForm.js loaded!");
+let angle = 0;
+const body = document.body;
 
-function getUserData(){
-  var user = document.getElementById("user").value;
-  var pass = document.getElementById("pass").value;
-
-  if(user == "admin" && pass == "admin"){
-    /*window.open("settings.html", "_self");*/
-    error("This function is under construction", 0);
-  }else{
-    error("Wrong Username and/or Password", 1);
-  }
-}
-
-function createUser(){
-  error("This function is under construction", 1);
-}
-
-function error(msg, s){
-  if(s == 0){
-    document.getElementById("error").style.color = "#00ffa2";
-  } else if (s == 1){
-    document.getElementById("error").style.color = "#ff003c";
-  }
-
-  document.getElementById("wmiddle").style.height = "225px";
-  document.getElementById("error").style.display = "block";
-  document.getElementById("error").innerHTML = msg;
-
-  setTimeout(function(){
-    document.getElementById("wmiddle").style.height = "200px";
-    document.getElementById("error").style.display = "none";
-  }, 3000);
-}
-
-
-/*Jquery*/
-var angle = 0; var test = $('body');
-
-function changeAngle() {
-    angle = (angle + 5) % 360;
-    test.css({
-        'background': 'linear-gradient(' + angle + 'deg,#00ff6e,#00fff7)',
-    });
+function changeAngle(){
+  angle = (angle + 5) % 360;
+  body.style.background = `linear-gradient(${angle}deg, #00ff6e, #00fff7)`
 }
 
 setInterval(changeAngle, 100);
